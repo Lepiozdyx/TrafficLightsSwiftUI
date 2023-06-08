f1b7d1cf0e7bf7043d77c77b6b36226c8841ccd0
Alex Zhulai <Lepiozdyx@gmail.com>
2023-06-08 17:13:08 -0300

    Setup project + add a view to describe the traffic light section

---
+++ TrafficLightsSwiftUI/LightSectionView.swift
@@ -0,0 +1,20 @@
+//
+//  LightSectionView.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Alex on 08.06.2023.
+//
+
+import SwiftUI
+
+struct LightSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LightSectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        LightSectionView()
+    }
+}
