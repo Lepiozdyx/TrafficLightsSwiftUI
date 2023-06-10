b81541541154b98b1689af9f3faf41230c61abac
Alex Zhulai <Lepiozdyx@gmail.com>
2023-06-10 16:33:57 -0300

    Setup UI + logic for the displayed UI elements

--- TrafficLightsSwiftUI/ContentView.swift
+++ TrafficLightsSwiftUI/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var startButtonText = "START"
+    @State private var currentLight: CurrentLight = .red
+    @State private var redOpacity = 0.3
+    @State private var yellowOpacity = 0.3
+    @State private var greenOpacity = 0.3
+    
     var body: some View {
         VStack {
-            LightSectionView(color: .red, text: "STOP")
-            LightSectionView(color: .orange, text: "WAIT")
-            LightSectionView(color: .green, text: "GO")
+            LightSectionView(color: .red, text: "STOP", opacity: redOpacity)
+            LightSectionView(color: .yellow, text: "WAIT", opacity: yellowOpacity)
+            LightSectionView(color: .green, text: "GO", opacity: greenOpacity)
             Spacer()
+            
+            Button(action: pressingButton) {
+                Text(startButtonText)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+            }
+                .frame(width: 200.0, height: 50.0)
+                .background(.blue)
+                .clipShape(Capsule())
         }
-        .padding(.top, 40)
+        .padding()
+    }
+    
+    private func pressingButton() {
+        if startButtonText == "START" {
+            startButtonText = "NEXT"
+        }
+        
+        switch currentLight {
+        case .red:
+            greenOpacity = 0.3
+            redOpacity = 1
+            currentLight = .yellow
+        case .yellow:
+            redOpacity = 0.3
+            yellowOpacity = 1
+            currentLight = .green
+        case .green:
+            yellowOpacity = 0.3
+            greenOpacity = 1
+            currentLight = .red
+        }
+        
+    }
+    
+}
+
+extension ContentView {
+    private enum CurrentLight {
+        case red, yellow, green
     }
 }
 
